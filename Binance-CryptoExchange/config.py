7a0e1de1f16a65efdc9309968c2440a95e32a3bd
Shubhankar Mayank <shubhankar1904@users.noreply.github.com>
2018-01-16 16:29:19 +0530

    key removed

--- Binance-CryptoExchange/config.py
+++ Binance-CryptoExchange/config.py
@@ -1,9 +1,9 @@
 
 
 def getApiKey():
-	apiKey = "GhQDowBtV9jgJf0UvgXnAS7tNLNCI20KABmGgNeh75vmTxPlb5lCV2eOS3SWf0QB"
+	apiKey = "asd"
 	return apiKey
 	
 def getSecretKey():	
-	secretKey=b'Ma5NnynEFJYPeT7j5a8RbkcXU4ULFiDuEgJvk1tDKEawPSiV9x07ksDphDnmM5wh'
+	secretKey=b'asd'
 	return secretKey
